1e58927e191c44203e07a7aefa439c69b9a76910
Allan Sham <allan.sham@trapezegroup.com>
2019-12-23 17:25:04 +0000

    Style christmas message

--- src/app/dialog-already-exists/dialog-already-exists.component.ts
+++ src/app/dialog-already-exists/dialog-already-exists.component.ts
@@ -9,6 +9,9 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 export class DialogAlreadyExistsComponent implements OnInit {
   headerMessage: string;
   message: string;
+  message1: string;
+  message2: string;
+  message3: string;
   buttonMessage: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
@@ -20,9 +23,13 @@ export class DialogAlreadyExistsComponent implements OnInit {
       this.buttonMessage = 'x';
     } else if (this.data === 'ChristmasMessage') {
       this.headerMessage = 'To 腐乳';
-      this.message =
-        // tslint:disable-next-line: max-line-length
-        `You did it, You successfully completed the 2019 Christmas Challenge! This is a Bill Splitter or you can call it a "Sharer". You can see two items in our "bill", behind this pop up box. Christmas is a time of joy and happiness, like this "bill" I hope we can "Share" many joyful moment between us. In the same way, we will also face many obstacles some as high as Mount Everest, I also hope we can "Share" our challenges and hardships between us. Your Christmas present is hidden in your room. I hope it can shine a light for you health as the star did for the wisemen 2000 plus years ago. Merry Christms and a Happy New Year.`;
+      this.message = `You did it, you successfully completed the 2019 Christmas Challenge! `;
+      // tslint:disable-next-line: max-line-length
+      this.message1 = `This is a Bill Splitter or you can call it a "Moments Sharer". Behind this pop up box you can see two items in our "bill". Christmas is a time of joy and happiness, like this "bill" I hope we can "share" many joyful moments between us. In the same way, we will also face many obstacles some as big as Mount Everest, I also hope we can "share" our challenges and hardships between us.`;
+      // tslint:disable-next-line: max-line-length
+      this.message2 = `Your Christmas present is hidden in your room, I hope it can shine a light for you health as the star did for the wisemen 2000 plus years ago.`;
+      this.message3 = `Merry Christms and a Happy New Year.`;
+
       this.buttonMessage = 'Love Allan';
     } else {
       this.headerMessage = 'Error: Already Exists';
